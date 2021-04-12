f7cbcbe5c3d904d3a3ee761c681405472ad9cca6
Adam Vlcek <vlce.on01@gmail.com>
2021-04-12 13:47:38 +0200

    test: login

--- tests/integration/auth/test_post_routes.py
+++ tests/integration/auth/test_post_routes.py
@@ -15,3 +15,17 @@ def test_post_registration(client, load_input_body, clear_json):
     assert post_res.status_code == 200
     assert fist_key == "accessToken"
 
+
+@pytest.mark.django_db()
+def test_post_login(client, load_input_body, clear_json):
+    input_data = load_input_body(__file__, "post_login.json")
+    user = Users(email=input_data["email"], first_name=input_data["first_name"], last_name=input_data["last_name"],
+                 phone=input_data["phone"], password=input_data["password"])
+    user.save()
+    post_res = client.post("/auth/login", input_data, content_type="application/json")
+    response_body = post_res.json()
+    clear_json(response_body)
+    fist_key = list(response_body.keys())[0]
+    assert post_res.status_code == 200
+    assert fist_key == "accessToken"
+
